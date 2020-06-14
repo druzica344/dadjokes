7cba1153111cf22ebb8d21ac722eb8c070752af5
Vi <druzica34@seznam.cz>
2020-06-14 22:15:46 +0200

    ukol c.15

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
+import Vue from "vue";
+import App from './components/app.vue';
 import './index.html';
-import './style.css';
 
 new Vue({
   el: "#app", 
-  data: {
-    upLikes: 0,
-    downLikes: 0
-  },
-  methods: {
-    likesUp(){
-      this.upLikes ++;
-    },
-    likesDown(){
-      this.downLikes ++;
-    }
-  }
-});
-
+  render: h => h(App),
+}); 
